31d563f608c37c5371e0bda5ae366fc75c9ef91d
pramod0514 <pramod.ukkali@oneorigin.us>
2024-08-04 20:01:55 +0530

    cors changes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ const dashboardRoutes = require('./routes/dashboard');
 const cors = require('cors'); // Import the cors package
 const connectDb = require('../MongoConfig');
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 
 // Use cors middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'], // Add your domain here
+    credentials: true, // Enable cookies in requests
+}));
 connectDb();
 // Middleware to parse JSON requests
 app.use(express.json());
