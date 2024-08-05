04eb26c31966c288e56903317b57cce10fda78dd
pramod0514 <pramod.ukkali@oneorigin.us>
2024-08-05 17:30:11 +0530

    filemanagement changes

--- src/routes/issues.js
+++ src/routes/issues.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const issueController = require('../controllers/issues');
 const multer = require('multer');
+const fs = require('fs'); // Import the file system module
+const path = require('path');
 const authMiddleware = require('../middleware/auth');
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const dir = 'uploads/';
+
+    // Check if the directory exists
+    if (!fs.existsSync(dir)) {
+      // Create the directory if it doesn't exist
+      fs.mkdirSync(dir, { recursive: true });
+      console.log(`Directory created: ${dir}`);
+    } else {
+      console.log(`Directory already exists: ${dir}`);
+    }
+
     console.log(`Saving file to: ${dir}`);
     cb(null, dir);
   },
